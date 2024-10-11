35cca929bbba9f5e1c7489c371292253b50da19b
Monalisa Onyango <+>
2024-10-11 14:02:40 +0300

    c and s lab

--- server/app.py
+++ server/app.py
@@ -23,12 +23,30 @@ def clear_session():
 @app.route('/articles')
 def index_articles():
 
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-@app.route('/articles/<int:id>')
+@app.route('/articles/<int:id>', methods=['GET'])
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+
+    session['page_views'] +=1
+
+    if session['page_views'] <= 3:
+
+        article = Article.query.filter(Article.id == id).first()
+
+        article_dict = article.to_dict()
+
+        response = make_response(article_dict,200)
+
+        return response
+
+        # return Article.query.filter(Article.id == id).first().to_dict(),200
+    
+    return make_response({'message': 'Maximum pageview limit reached'},401)
+
 
 if __name__ == '__main__':
     app.run(port=5555)
